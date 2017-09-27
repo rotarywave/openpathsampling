60c008654508e483cdf59ed858324ecd99d8cea2
Anastasia Maslechko <maslechko.an@gmail.com>
2017-09-27 14:24:11 +0200

    import corrected

--- openpathsampling/analysis/interface_optimization.py
+++ openpathsampling/analysis/interface_optimization.py
@@ -5,37 +5,34 @@ Important methods defined here
 
 find_interface_and_cross
     Reads the output of the analyze-tool after a certain number of cycles
-    (defined by the user). Information about 'transition' and 'histograms' must be provided.
+    (defined by the user). Information about 'transition' must be provided.
 
 get_interpolation_points:
     A method to calculate the points of the transformation, that is going to map required
     sequence to the new values of the interface positions.
 
-new_interface_A
-    A method for calculation of the new interface positions, using I approach: the number of interfaces
-    kept the same, only the values of the positions are going to be changed.
+save_n_interfaces
+    A method for calculation of the new interface positions, using I approach:
+    the number of interfaces kept the same, only the values of the positions are
+    going to be changed.
 
-new_interface_B
+save_p_interfaces
     A method for calculation of the new interface positions, using II approach: the number of
     the interfaces might be changed, "expected" crossing probability for each ensemble has a
     predefined value.
 
 """
-import matplotlib.pyplot as plt
 import numpy as np
-import openpathsampling as paths
 from scipy.interpolate import interp1d
-from openpathsampling.numerics import SparseHistogram
 
 def find_interface_and_cross(transition):  # pragma: no cover
-    """Read the output of the analyze tool, and get interface positions and crossing probabilities.
-    Example for getting input:
+    """To read the output of the analysis tool, and get interface positions
+    and crossing probabilities. Example to get input:
     transition = system.transitions[(stateA, stateB)]
-    histo = transition.histograms['max_lambda']
 
     Parameters
     ----------
-    transition : an object
+    transition : an object like :py:class:`TISTransition`
         Information about transition
 
     Returns
@@ -45,20 +42,28 @@ def find_interface_and_cross(transition):  # pragma: no cover
     old_crossing_probabilities : list of floats
         Crossing probabilities for each ensemble
     """
-    histo = transition.histograms['max_lambda']
-    old_interfaces = transition.interfaces.lambdas
-    all_probabilities = []
-    for item in histo:
-        all_probabilities.append(histo[item].reverse_cumulative())
-
-    old_crossing_probabilities = []
-    for item in old_interfaces:
-        old_crossing_probabilities.append(all_probabilities[1](item))
-    return old_interfaces, old_crossing_probabilities
+    try:
+        trans_hists = transition.histograms['max_lambda']
+        all_hist_functions = {}
+        old_interfaces = []
+        old_probabilities = []
+        for ensemble in trans_hists:
+            hist_function = trans_hists[ensemble].reverse_cumulative()
+            all_hist_functions[ensemble.interface.lambda_max] = hist_function
+        sorted_keys = sorted(all_hist_functions.keys())
+        for i, lambdai in enumerate(sorted_keys):
+            prob_func = all_hist_functions[sorted_keys[i - 1]]
+            prob = prob_func(lambdai)
+            old_interfaces.append(lambdai)
+            old_probabilities.append(prob)
+        return old_interfaces, old_probabilities
+    except TypeError:
+        msg = 'Inappropriate input: transition should be an obj like `TISTransition`'
+        print (msg)
 
 
 def get_interpolation_points(pos_interfaces, cross_probabilities):
-    """Calculated the point for transformation funstion.
+    """Calculated the point for transformation function.
 
     Parameters
     ----------
@@ -70,22 +75,58 @@ def get_interpolation_points(pos_interfaces, cross_probabilities):
     Returns
     -------
     out[0] : list of floats
-        x-values for interpolation
+        x-values for interpolation, related to the crossing probabilities
     out[1] : list of floats
-        y-values for interpolation
+        y-values for interpolation, related to the interfaces.
     """
-    
     logarithms = np.log(cross_probabilities)
     point_values = []
     for i in range(len(pos_interfaces)):
-        point_values.append(sum(logarithms[:(i+1)])/float(sum(logarithms)))
-    # now create a set of base points for interpolation function
-    x = point_values[:]
-    y = pos_interfaces[:]
-    return x, y
-        
+        point_values.append(sum(logarithms[:(i + 1)]) / float(sum(logarithms)))
+    return [point_values, pos_interfaces]
 
-def new_interfaces_A(interfaces, cross_probabilities):
+
+def _round(value, formatting):
+    """Changes the format the value accordingly to the string formatting type.
+    Basically used to reduce precision in defining of the number.
+
+    Parameters
+    ----------
+    value : float or int
+        Floating point number to change
+    formatting : string
+        String-type python keyword to define a formatting of the number,
+        like '.4g', '.2f' etc.
+
+    Returns
+    -------
+    out : float
+        The value in the desired format.
+    """
+    result = float(format(value, formatting))
+    return result
+
+
+def _mapping_f(interfaces, cross_probabilities):
+    """Defines a mapping function for the transformation.
+    It should be monotonically increasing.
+
+    Parameters
+    ----------
+    interfaces : list of floats
+        Current interfaces from the simulation.
+    cross_probabilities : list of floats
+        Current crossing probabilities in the simulation. All values should be non-zero.
+
+    Returns
+    -------
+    out : func
+        A mapping function"""
+    prob_points, interface_points = get_interpolation_points(interfaces, cross_probabilities)
+    return interp1d(prob_points, interface_points, kind='linear')
+
+
+def save_n_interfaces(interfaces, cross_probabilities, fixed_n=None, formatting='.4g'):
     """Calculates the new interfaces, if the number of the interfaces remains the same.
 
     Parameters
@@ -97,30 +138,35 @@ def new_interfaces_A(interfaces, cross_probabilities):
 
     Returns
     -------
-    new_interfaces : list of floats
+    out : list of floats
         New positions of the interfaces
     """
-    
-    x_flambda, y_lambda = get_interpolation_points(interfaces, cross_probabilities)
+    prob_points = get_interpolation_points(interfaces, cross_probabilities)[0]
     # build transformation function
-    f = interp1d(x_flambda, y_lambda, kind='linear')
-    # define the number of interfaces
-    if len(interfaces) < 3:
-        N = 3
+    mapping_f = _mapping_f(interfaces, cross_probabilities)
+    result = []
+    if fixed_n is None:
+        number_of_interfaces = len(interfaces)
     else:
-        N = len(interfaces)
+        number_of_interfaces = fixed_n
+    if number_of_interfaces < 3:
+        number_of_interfaces = 3  # number of the interfaces cannot be less than 3
+    prob_step = (prob_points[-1] - prob_points[0]) / float(number_of_interfaces - 1)
+    for i in range(number_of_interfaces):
+        if i == 0:
+            value = mapping_f(prob_points[0])
+        elif i == (number_of_interfaces - 1):
+            value = mapping_f(prob_points[-1])
+        else:
+            arg = prob_points[0] + prob_step * i
+            value = mapping_f(arg)
+        result.append(_round(float(value), formatting))
+    return result
 
-    # find the interfaces
-    f_interval = ( x_flambda[-1] - x_flambda[0] ) / float(N-1)
-    new_interfaces = []
-    for i in range(N):
-        value = f(x_flambda[0] + f_interval * i)
-        new_interfaces.append(float(value))
-    return new_interfaces
 
-
-def new_interfaces_B(interfaces, cross_probabilities, p=0.5):
-    """Calculates the new interfaces, if the crossing probabilities expected to have some fixed value.
+def save_p_interfaces(interfaces, cross_probabilities, fixed_p=0.4, formatting='.4g'):
+    """Calculates the new interfaces, if the crossing probabilities
+    expected to have some fixed value.
 
     Parameters
     ----------
@@ -128,30 +174,32 @@ def new_interfaces_B(interfaces, cross_probabilities, p=0.5):
         Old interface positions
     cross_probabilities : list of floats
         Old crossing probabilities for each ensemble
-    p : float
-        Expected value for crossing probabilities in each ensemble. Default 0.5.
+    fixed_p : float
+        Expected value for crossing probabilities in each ensemble. Default is 0.4.
 
     Returns
     -------
-    new_interfaces : list of floats
+    out : list of floats
         New positions of the interfaces
     """
-
-    x_flambda, y_lambda = get_interpolation_points(interfaces, cross_probabilities)
+    prob_points = get_interpolation_points(interfaces, cross_probabilities)[0]
     # build interpolation function
-    f = interp1d(x_flambda, y_lambda, kind='linear')
-    # find the number of interfaces
+    mapping_f = _mapping_f(interfaces, cross_probabilities)
+    result = []
     total_cross_prob = 1
     for item in cross_probabilities:
         total_cross_prob = total_cross_prob * item
-    if (int(np.log(total_cross_prob) / (np.log(p)) + 1)) < 3:
-        N = 3 # number of the interfaces cannot be less than 3
-    else:
-        N = int(np.log(total_cross_prob) / np.log(p)) + 1
-    # find the interfaces
-    f_interval = ( x_flambda[-1] - x_flambda[0] ) / float(N-1)
-    new_interfaces = []
-    for i in range(N):
-        value = f(x_flambda[0] + f_interval * i)
-        new_interfaces.append(float(value))
-    return new_interfaces
+    number_of_interfaces = int(np.log(total_cross_prob) / np.log(fixed_p)) + 2
+    if number_of_interfaces < 3:
+        number_of_interfaces = 3  # number of the interfaces cannot be less than 3
+    prob_step = (prob_points[-1] - prob_points[0]) / float(number_of_interfaces - 1)
+    for i in range(number_of_interfaces):
+        if i == 0:
+            value = mapping_f(prob_points[0])
+        elif i == (number_of_interfaces - 1):
+            value = mapping_f(prob_points[-1])
+        else:
+            arg = prob_points[0] + prob_step * i
+            value = mapping_f(arg)
+        result.append(_round(float(value), formatting))
+    return result
--- openpathsampling/tests/test_interface_optimization.py
+++ openpathsampling/tests/test_interface_optimization.py
@@ -1,65 +1,78 @@
-from interface_optimization import new_interfaces_A, new_interfaces_B, get_interpolation_points
+from openpathsampling.analysis.interface_optimization import (save_n_interfaces, 
+                                                              save_p_interfaces, 
+                                                              get_interpolation_points)
 from nose.tools import raises, assert_equal
 from numpy.testing import assert_allclose
-
+import numpy.random as random
 
 # nosetests --with-coverage --cover-erase --cover-package=interface_optimization --cover-html test_interface_optimization.py
 #
 
 class TestInterfaceOptimization(object):
 
-    def test_function_A(self):
+    def test_function_n(self):
         interfaces = [0, 1, 2, 3]
         cross_prob = 0.5
         probabilities = [1., cross_prob, cross_prob, cross_prob]
-        new_I = new_interfaces_A(interfaces, probabilities)
+        new_I = save_n_interfaces(interfaces, probabilities)
         assert_allclose(interfaces, new_I)
 
-    def test_funcA_exception(self):
+
+    def test_func_n_exception(self):
         interfaces = [0, 2]
         cross_prob = [1.0, 0.3]
-        new_int = new_interfaces_A(interfaces, cross_prob)
+        new_int = save_n_interfaces(interfaces, cross_prob)
         assert_equal(3, len(new_int))
+
+
+    def test_func_n_null(self):
+        interfaces = [0, 2, 4, 6]
+        cross_prob = [1.0, 0.5, 0.5, 0.5]
+        new_int = save_n_interfaces(interfaces, cross_prob, fixed_n=5)
+        assert_equal(5, len(new_int))
     
      
-    def test_function_B(self):
+    def test_function_p(self):
         interfaces = [0, 2, 4, 6, 8, 10, 12]
         test_cross = 0.2
-        prob = [1.]
-        for item in interfaces[1:]:
-            prob.append(test_cross)
-        new_int_app_2 = new_interfaces_B(interfaces, prob, test_cross)
+        prob = [0.2] * len(interfaces)
+        for i, item in enumerate(interfaces[1:]):
+            prob[i + 1] = test_cross
+        new_int_app_2 = save_p_interfaces(interfaces, prob, test_cross)
         N = len(new_int_app_2)
-        interval = (interfaces[-1] - interfaces[0]) / (N-1)
-        artificial_input = []
-        for item in range(N):
-            artificial_input.append(interfaces[0] + item * interval)
+        interval = (interfaces[-1] - interfaces[0]) / float(N-1)
+        artificial_input = [interfaces[0] + i * interval for i in range(N)]
         assert_allclose(artificial_input, new_int_app_2)
 
-    def test_function_B_int(self):
+
+    def test_function_p_int(self):
         interfaces = [0, 3, 6, 9, 12]
         test_cross = 0.8
-        prob = [1.]
-        for item in interfaces[1:]:
-            prob.append(test_cross)
-        new_int = new_interfaces_B(interfaces, prob, 0.6)
-        middle = ( interfaces[-1] - interfaces[0] ) / 2.0
+        prob = [1.] * len(interfaces)
+        for i, item in enumerate(interfaces[1:]):
+            prob[i + 1] = test_cross
+        new_int = save_p_interfaces(interfaces, prob, 0.6)
+        middle = (interfaces[-1] - interfaces[0]) / 2.0
         artificial_input = [interfaces[0], middle, interfaces[-1]]     
         assert_equal(new_int, artificial_input)
 
+
+    def test_func_p_3(self):
+        interfaces = [0, 2]
+        cross_prob = [1.0, 0.9]
+        new_int = save_p_interfaces(interfaces, cross_prob, fixed_p=0.1)
+        print (new_int)
+        assert_equal(3, len(new_int))
+
+
     def test_interpolation_points(self):
         interfaces = [0, 1, 2, 3, 4]
-        crossing_probabilities = [0.8, 0.7, 0.9, 0.5, 0.2]
-        x, y = get_interpolation_points(interfaces, crossing_probabilities)
-
-        interfaces = [0, 1, 2, 3, 4]
-        crossing_probabilities = []
-        import numpy.random as random
-        for item in interfaces:
-            crossing_probabilities.append((random.random()+0.001)*0.99)
+        crossing_probabilities = [0] * len(interfaces)
+        for i, item in enumerate(interfaces):
+            crossing_probabilities[i] = (random.random()+0.001)*0.99
         x, y = get_interpolation_points(interfaces, crossing_probabilities)
         # x has to be monotonicaly increasing array of values
         is_monotonic = True
         for i in range(len(x)-1):
-            is_monotonic = ( is_monotonic and ( x[i] <= x[i + 1] ) )        
+            is_monotonic = (is_monotonic and (x[i] <= x[i + 1]))
         assert_equal(True, is_monotonic)
